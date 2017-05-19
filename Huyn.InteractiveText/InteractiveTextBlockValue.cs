09ac7d70353d08097562ba2d793747c4494148ca
Rudy Huyn <rudy@dropbox.com>
2017-05-18 17:21:53 -0700

    Initial commit

---
+++ Huyn.InteractiveText/InteractiveTextBlockValue.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+
+
+namespace Huyn.InteractiveText
+{
+    public class InteractiveTextBlockValue : INotifyPropertyChanged
+    {
+
+
+        #region Text
+
+        private string _text;
+        public string Text
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                if (_text == value)
+                    return;
+                _text = value;
+                OnPropertyChanged(nameof(Text));
+            }
+
+        }
+
+        #endregion
+
+        #region Action
+        public string Action { get; set; }
+
+        #endregion
+
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+    }
+}
